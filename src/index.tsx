109968b37942e5b68b145c7bcb4667c5e98e304c
agiveygives <agivens@justworks.com>
2023-02-24 09:49:41 -0600

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,45 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import classNames from 'classnames';
+import { Formik } from 'formik';
+import i18n from 'i18next';
+import resourcesToBackend from 'i18next-resources-to-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
+import Button from './components/Button';
+
+classNames();
+Formik({
+  initialValues: {},
+  onSubmit: () => {}
+});
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(resourcesToBackend((
+    language: string,
+    namespace: string,
+    callback: (error: Error | null, resources: Record<string, string> | null) => void
+  ) => {
+    import(`./locales/${language}/${namespace}.json`)
+      .then((resources) => {
+        callback(null, resources)
+      })
+      .catch((error) => {
+        callback(error, null)
+      })
+  }))
+  .init({
+    fallbackLng: 'en-US',
+    interpolation: {
+      escapeValue: false // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+    },
+    react: {
+      useSuspense: true
+    }
+  });
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,6 +49,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
+    <Button />
   </React.StrictMode>
 );
 
